90cf0e89ca19f0f0aa43e17d2a209da8c85401aa
Ayaoba <abolarinesther98@gmail.com>
2022-06-25 16:08:56 +0100

    first commit

---
+++ name.js
@@ -0,0 +1,12 @@
+var firstName = 'Esther';
+var middleName = 'Abosede';
+var lastName = 'Abolarin';
+var height = '6 feet';
+var country = 'Nigeria';
+
+let name =firstName + '' + middleName + '' +lastName;
+
+document.write('Name:' + ''+name + '<br>');
+document.write('Height:' + ''+height + '<br>');
+document.write('Country:' + ''+country);
+
